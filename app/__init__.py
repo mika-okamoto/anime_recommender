4d137bdbbb12d1c8f647a16b77a6fe1cea385fdd
mika-okamoto <mika.disneypins@gmail.com>
2023-12-23 07:15:51 +0900

    initial page layout / backend

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,7 @@
 import os
 
-from flask import Flask, render_template
-import numpy as np
+from flask import Flask, render_template, request
 import pandas as pd
-from sklearn.metrics.pairwise import cosine_similarity
-import re
-from scipy.sparse import csr_matrix
-from sklearn.neighbors import NearestNeighbors
 import pickle
 
 
@@ -26,41 +21,34 @@ def create_app(test_config=None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
-
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
     
-    @app.route('/')
+    @app.route('/', methods = ("GET", "POST"))
     def index():
+        recs = []
+        if request.method == "POST":
+            recs = []
+            anime_name = request.form.get('anime-name')
+            with open("app/static/data/anime_recommender.pkl", 'rb') as file:  
+                model = pickle.load(file)
+
+            anime_names = pd.read_csv('app/static/data/anime_names.csv')
+
+            try:
+                query_index = anime_names[anime_names['name'] == anime_name].index[0]
+                anime = pd.read_csv('app/static/data/anime_pivot.csv', skiprows = lambda x : x != query_index + 1, nrows=1, header=None)
+                distances, indices = model.kneighbors(anime, n_neighbors = 6)
+
+                for i in range(1, len(distances.flatten())):
+                    recs.append('{0}: {1}'.format(i, anime_names.loc[indices.flatten()[i]][0]))
+                    # print('{0}: {1}, with distance of {2}:'.format(i, anime_names.loc[indices.flatten()[i]][0], distances.flatten()[i]))
+                return render_template("index.html", anime_name = anime_name, recs = recs)
+            except: 
+                return render_template("index.html", anime_name = anime_name)
+            
         return render_template("index.html")
-    
-    @app.route('/rec')
-    def rec():
-
-        with open("app/static/data/anime_recommender.pkl", 'rb') as file:  
-            model = pickle.load(file)
-
-        anime_names = pd.read_csv('app/static/data/anime_names.csv')
-
-        anime_name = 'One Punch Man'
-        query_index = anime_names[anime_names['name'] == anime_name].index[0]
-
-        anime = pd.read_csv('app/static/data/anime_pivot.csv', skiprows = lambda x : x != query_index + 1, nrows=1, header=None)
-
-        distances, indices = model.kneighbors(anime, n_neighbors = 6)
-        toreturn = ''
-
-        for i in range(0, len(distances.flatten())):
-            if i == 0:
-                toreturn += 'Recommendations for {0}:\n'.format(anime_name)
-                print('Recommendations for {0}:\n'.format(anime_name))
-            else:
-                toreturn += '{0}: {1}, with distance of {2}:\n'.format(i, anime_names.loc[indices.flatten()[i]][0], distances.flatten()[i])
-                print('{0}: {1}, with distance of {2}:'.format(i, anime_names.loc[indices.flatten()[i]][0], distances.flatten()[i]))
-        return toreturn
-        # turn this into a html page with a form to input anime name
-        # turn into list & do the loop html thing about it??
 
     return app
+
+
+# need anime lookup page/table (query from csv for idx name based on anime_names.csv)
+# ideally search for words (non case sensitive) & translate between engl/japanese to show either
